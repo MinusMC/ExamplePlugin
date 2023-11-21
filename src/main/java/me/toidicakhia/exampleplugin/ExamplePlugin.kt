7eeed2dead49c84833badf22bfa44dd53d091be4
toidicakhia <toidicakhia1234@gmail.com>
2023-11-21 13:16:38 +0700

    fix

--- src/main/java/me/toidicakhia/exampleplugin/ExamplePlugin.kt
+++ src/main/java/me/toidicakhia/exampleplugin/ExamplePlugin.kt
@@ -1,8 +1,9 @@
 package me.toidicakhia.ExamplePlugin
 
 import net.minusmc.minusbounce.plugin.Plugin
+import net.minusmc.minusbounce.plugin.PluginAPIVersion
 
-class ExamplePlugin: Plugin("ExamplePlugin", "dev") {
+class ExamplePlugin: Plugin("ExamplePlugin", "dev", PluginAPIVersion.VER_01) {
 	override fun init() {
 		println("This is an example mod!!!")
 	}
